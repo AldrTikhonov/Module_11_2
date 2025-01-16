af2382768bce7fb4be61caf9d1895def62f00b45
Alexander Tikhonov <AldrTikhonov>
2025-01-16 20:43:59 +0300

    add module_venv.py

---
+++ module_venv.py
@@ -0,0 +1,8 @@
+import os
+
+print("python3 -m venv new_venv")
+print("source new_venv/bin/activate")
+
+print(os.getcwd())
+os.chdir('/home/user/Module_11_2/pygame_example.git')
+print(os.getcwd())
